0da46d9170ae82e5159cd5cca3b00692a6d2e607
Hamza Mehmood <hamza.mehmood@zmwl.local>
2026-01-07 12:44:41 +0100

    Faker in factory fix 2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -12,33 +13,52 @@ use Illuminate\Support\Str;
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * The name of the factory’s corresponding model.
      */
-    protected static ?string $password;
+    protected $model = User::class;
 
     /**
-     * Define the model's default state.
+     * Define the model’s default state.
      *
      * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
+            // Random name
             'name' => $this->faker->name(),
+
+            // Unique safe email
             'email' => $this->faker->unique()->safeEmail(),
+
+            // Use current timestamp for email verified
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+
+            // Default hashed password
+            'password' => Hash::make('password'),
+
+            // Remember token
             'remember_token' => Str::random(10),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the model’s email address should be unverified.
      */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user should be an admin.
+     */
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_admin' => true,
         ]);
     }
 }
